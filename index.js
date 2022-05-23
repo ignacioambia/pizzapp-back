c469f1686a0e5f62feb9fdc78980095b4e3a3683
ignacio.ambia <ignacio.ambia@gmail.com>
2022-05-22 19:06:34 -0500

    read port variable

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res)=>{
  res.send('This is the first response');
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
  console.log('Port is running');
