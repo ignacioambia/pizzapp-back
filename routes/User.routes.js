e38f39b1a5a498b400333c4968d6d7729c38ad9e
ignacio.ambia <ignacio.ambia@gmail.com>
2022-07-27 19:56:00 -0500

    add /update_delays endpoint

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -2,6 +2,30 @@ const express = require("express");
 const User = require("../schemas/User.schema");
 const route = express.Router();
 
+function updateDelay({ user_id, num_delays }) {
+  return new Promise(async (resolve, reject) => {
+    await User.findOne({ _id: user_id }, async (err, user) => {
+      if (err) reject(err);
+
+      if (num_delays >= 0) {
+        const newDelays = Array.from(Array(num_delays), () => new Date());
+        user.delays = [...user.delays, ...newDelays];
+      } else {
+        user.delays.splice(
+          user.delays.length + num_delays,
+          Math.abs(num_delays)
+        );
+      }
+      await user.save(function (err) {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+    }).clone();
+  });
+}
+
 route.get("/", async (req, res) => {
   const allUsers = await User.find();
   res.send(allUsers);
@@ -24,10 +48,18 @@ route.put("/add_delay", async (req, res) => {
   const { user_id } = req.body;
   const updateUser = await User.findOneAndUpdate(
     { _id: user_id },
-    { $push: { delays: new Date() } },
+    { $push: { delays: { $each: [1, 2, 3] } } },
     { new: true }
   );
   res.send(updateUser);
+});
+
+route.put("/update_delays", async (req, res) => {
+  const functions = req.body.map((element) => updateDelay(element));
+  Promise.all(functions).then(async () => {
+    console.log("all promises were fulfilled");
+    res.send(await User.find({}));
+  });
 });
 
 route.put("/remove_delay", async (req, res) => {
